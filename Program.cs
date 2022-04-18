39f49806ee42bd3b14fd0c5cd2b8de6ab2378a40
Fabio Lazaro de Almeida <fabioalmeida2004@gmail.com>
2022-04-18 16:33:39 -0300

    verificação idade

--- Program.cs
+++ Program.cs
@@ -6,8 +6,11 @@ using Cadastro_Pessoa_SA2___ER2.Classes;
 
 PessoaFisica novaPf = new PessoaFisica();
 
-novaPf.nome = "Fabio";
+// novaPf.nome = "Fabio";
 
-//cwl snippet
-Console.WriteLine("Nome: " + novaPf.nome);
-Console.WriteLine($"Nome: {novaPf.nome}");
+// //cwl snippet
+// Console.WriteLine("Nome: " + novaPf.nome);
+// Console.WriteLine($"Nome: {novaPf.nome}");
+
+
+novaPf.ValidarDataNascimento("01,01,2000");
