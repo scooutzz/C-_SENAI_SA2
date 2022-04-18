d2cc8a764ce51a4a39f2a44e3612d1f9e166dadd
Fabio Lazaro de Almeida <fabioalmeida2004@gmail.com>
2022-04-18 16:56:23 -0300

    endereço

---
+++ Classes/Endereco.cs
@@ -0,0 +1,15 @@
+namespace Cadastro_Pessoa_SA2___ER2.Classes
+{
+    public class Endereco
+    {
+        public string ?logradouro { get; set; }
+        
+        public int numero { get; set; }
+        
+        public string ?complemento { get; set; }
+        
+        public bool endComercial { get; set; }
+        
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,12 +5,25 @@ using Cadastro_Pessoa_SA2___ER2.Classes;
 
 
 PessoaFisica novaPf = new PessoaFisica();
+Endereco novoEnd = new Endereco();
 
-// novaPf.nome = "Fabio";
+novaPf.nome = "Fabio";
+novaPf.dataNascimento = "07/08/2004";
+novaPf.cpf = "1234567890";
+novaPf.rendimento = 15000.5f;
 
-// //cwl snippet
-// Console.WriteLine("Nome: " + novaPf.nome);
+novoEnd.logradouro = "Alameda Barão de Limeira";
+novoEnd.numero = 539;
+novoEnd.complemento = "SENAI Informatica";
+novoEnd.endComercial = true;
+
+novaPf.endereco = novoEnd;
+
 // Console.WriteLine($"Nome: {novaPf.nome}");
+// Console.WriteLine($"Endereco: {novaPf.endereco.logradouro}, {novaPf.endereco.numero}");
 
+Console.WriteLine(@$"
+Nome: {novaPf.nome}
+Endereco: {novaPf.endereco.logradouro}, {novaPf.endereco.numero}
+");
 
-novaPf.ValidarDataNascimento("01,01,2000");
